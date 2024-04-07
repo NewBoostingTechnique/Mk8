35510ba3efab3f971f3bc7029bef980aaf7f556f
Russell Horwood <russell.horwood@gmail.com>
2024-04-07 13:19:08 +0100

    Don't need to check authorization on every route activation

--- Web/App/Router.jsx
+++ Web/App/Router.jsx
@@ -17,96 +17,94 @@ const PlayerDetail = lazy(() => import('../Players/PlayerDetail.jsx'));
 const PlayerList = lazy(() => import('../Players/PlayerList.jsx'));
 const TimeCreate = lazy(() => import('../Times/TimeCreate.jsx'));
 
-const authorizationClient = useAuthorizationClient();
+const authorizationPromise = useAuthorizationClient().getAsync();
 const courseClient = useCourseClient();
 const countryClient = useCountryClient();
 const playerClient = usePlayerClient();
 const proofClient = useProofTypeClient();
 
-// TODO Don't need to check authorization on every route activation
-
 const router = createBrowserRouter([
-  {
-    element: <App />,
-    errorElement: <ErrorBoundary />,
-    loader: getLocaleNameAsync,
-    children: [
-      {
-        path: '/',
-        element: <NewsList />
-      },
-      {
-        path: '/authorization/',
-        element: <Authorization />
-      },
-      {
-        path: '/news/',
-        element: <NewsList />
-      },
-      {
-        path: '/player/',
-        element: <PlayerList />,
-        loader: async () => {
-          const [authorization, players] = await Promise.all([
-            authorizationClient.getAsync(),
-            playerClient.listAsync()
-          ]);
-          return ({
-            authorization: authorization,
-            players: players
-          });
-        }
-      },
-      {
-        path: '/player/create/',
-        element: <PlayerCreate />,
-        loader: async () => {
-          const [countries, proofTypes] = await Promise.all([
-            countryClient.listAsync(),
-            proofClient.listAsync()
-          ]);
-          return ({
-            countries: countries,
-            proofTypes: proofTypes
-          });
-        }
-      },
-      {
-        path: '/player/detail/:playerName/',
-        element: <PlayerDetail />,
-        loader: async ({ params }) => {
-          const [authorization, player] = await Promise.all([
-            authorizationClient.getAsync(),
-            playerClient.detailAsync(params.playerName)
-          ]);
-          return ({
-            authorization: authorization,
-            player: player
-          });
-        }
-      },
-      {
-        path: '/time/create/:playerName/:courseName/',
-        element: <TimeCreate />,
-        loader: async ({ params }) => {
-          const [players, localeName, courses] = await Promise.all([
-            playerClient.listAsync(),
-            getLocaleNameAsync(),
-            courseClient.listAsync()
-          ]);
-          return ({
-            courseName: params.courseName,
-            courses: courses,
-            localeName: localeName,
-            playerName: params.playerName,
-            players: players
-          });
-        }
-      }
-    ]
-  }
+    {
+        element: <App />,
+        errorElement: <ErrorBoundary />,
+        loader: getLocaleNameAsync,
+        children: [
+            {
+                path: '/',
+                element: <NewsList />
+            },
+            {
+                path: '/authorization/',
+                element: <Authorization />
+            },
+            {
+                path: '/news/',
+                element: <NewsList />
+            },
+            {
+                path: '/player/',
+                element: <PlayerList />,
+                loader: async () => {
+                    const [authorization, players] = await Promise.all([
+                        authorizationPromise,
+                        playerClient.listAsync()
+                    ]);
+                    return ({
+                        authorization: authorization,
+                        players: players
+                    });
+                }
+            },
+            {
+                path: '/player/create/',
+                element: <PlayerCreate />,
+                loader: async () => {
+                    const [countries, proofTypes] = await Promise.all([
+                        countryClient.listAsync(),
+                        proofClient.listAsync()
+                    ]);
+                    return ({
+                        countries: countries,
+                        proofTypes: proofTypes
+                    });
+                }
+            },
+            {
+                path: '/player/detail/:playerName/',
+                element: <PlayerDetail />,
+                loader: async ({ params }) => {
+                    const [authorization, player] = await Promise.all([
+                        authorizationPromise,
+                        playerClient.detailAsync(params.playerName)
+                    ]);
+                    return ({
+                        authorization: authorization,
+                        player: player
+                    });
+                }
+            },
+            {
+                path: '/time/create/:playerName/:courseName/',
+                element: <TimeCreate />,
+                loader: async ({ params }) => {
+                    const [players, localeName, courses] = await Promise.all([
+                        playerClient.listAsync(),
+                        getLocaleNameAsync(),
+                        courseClient.listAsync()
+                    ]);
+                    return ({
+                        courseName: params.courseName,
+                        courses: courses,
+                        localeName: localeName,
+                        playerName: params.playerName,
+                        players: players
+                    });
+                }
+            }
+        ]
+    }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+    <RouterProvider router={router} />
 );
