6ea2fa5da6262f3e6fa5c88a7221027c55a752f0
Russell Horwood <russell.horwood@gmail.com>
2024-10-10 22:05:39 +0100

    Done

--- src/Core/News/New.cs
+++ src/Core/News/New.cs
@@ -2,8 +2,6 @@ namespace Mk8.Core.News;
 
 public record New
 {
-    // TODO: All entities (records) should only have get, and init (no set).
-
     public Ulid? Id { get; internal init; }
 
     public required string Title { get; init; }
