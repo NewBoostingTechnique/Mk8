d69a82dc64072426dafe09abe1f8e0122dd9cbfd
Russell Horwood <russell.horwood@gmail.com>
2025-01-13 14:57:24 +0000

    Database deployed

--- src/MySql.Console/Deploy.cs
+++ src/MySql.Console/Deploy.cs
@@ -15,23 +15,33 @@ internal class DeployCommand(
         Assembly assembly = Assembly.GetEntryAssembly()
             ?? throw new InvalidOperationException("Failed to get the entry assembly while determining the path to SQL scripts.");
 
+        // Require the mk8 and root connection strings to be on the same server.
         MySqlConnectionStringBuilder mk8ConnectionString = new(options.Value.ConnectionString);
+        MySqlConnectionStringBuilder rootConnectionString = new(options.Value.RootConnectionString);
+        if (mk8ConnectionString.Server != rootConnectionString.Server)
+            throw new InvalidOperationException("The root and MK8 connection strings must have the same server.");
 
+        // Connect to the db server as root.
         using MySqlConnection rootConnection = new(options.Value.RootConnectionString);
         await rootConnection.OpenAsync();
 
+        // Create the mk8 database.
         using MySqlCommand command = new($"CREATE DATABASE IF NOT EXISTS {mk8ConnectionString.Database};", rootConnection);
         await command.ExecuteNonQueryAsync();
 
-        command.CommandText = $"CREATE USER IF NOT EXISTS 'mk8'@'%%' IDENTIFIED BY '{mk8ConnectionString.Password}';";
+        // Create the mk8 app user.
+        command.CommandText = $"CREATE USER IF NOT EXISTS '{mk8ConnectionString.UserID}'@'%%' IDENTIFIED BY '{mk8ConnectionString.Password}';";
         await command.ExecuteNonQueryAsync();
-
         command.CommandText = $"USE {mk8ConnectionString.Database}; GRANT EXECUTE ON * TO 'mk8'@'%%';";
         await command.ExecuteNonQueryAsync();
 
-        using MySqlConnection mk8Connection = new(options.Value.ConnectionString);
+        // Connect to the mk8 database as the root user (to deploy the schema).
+        mk8ConnectionString.UserID = rootConnectionString.UserID;
+        mk8ConnectionString.Password = rootConnectionString.Password;
+        using MySqlConnection mk8Connection = new(mk8ConnectionString.ConnectionString);
         await mk8Connection.OpenAsync();
 
+        // Deploy the schema.
         await deployTable("course", "Courses/CourseTable.sql");
         await deployTable("country", "Countries/CountryTable.sql");
         await deployTable("region", "Regions/RegionTable.sql");
